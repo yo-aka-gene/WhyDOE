de7e0610b217ccf98f8a5e5d9db084bcce2c032d
yo-aka-gene <yujiokano@keio.jp>
2025-05-05 19:01:15 +0900

    :recycle::sparkles: add anova power

--- doe_modules/simulation/__init__.py
+++ doe_modules/simulation/__init__.py
@@ -1,20 +1,29 @@
 from ._abstract import AbstractSimulator
 from ._sim1 import Sim1
+from ._sim1_minus import Sim1Minus
+from ._sim1_plus import Sim1Plus
 from ._circuit import Circuit
+from ._circuit_minus import CircuitMinus
+from ._circuit_plus import CircuitPlus
 from ._sparse import Sparse
 from ._prototype import Prototype
 from ._test4 import Test4
 from ._nonlinear import NonLinear
 from ._mlr import MLR
+from ._anova_power import ff_anova_power
 
 
 __all__ = [
     AbstractSimulator, 
     Sim1,
+    Sim1Minus,
     Circuit,
+    CircuitMinus,
+    CircuitPlus,
     Sparse,
     Prototype,
     Test4,
     # NonLinear, 
-    MLR
+    MLR,
+    ff_anova_power,
 ]
