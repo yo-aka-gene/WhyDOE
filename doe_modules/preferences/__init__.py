3f61b1123fa32cda361ffc4cad313df18603f1bc
yo-aka-gene <yujiokano@keio.jp>
2025-05-05 18:56:56 +0900

    :recycle: add order2_interaction_regex

--- doe_modules/preferences/__init__.py
+++ doe_modules/preferences/__init__.py
@@ -1,12 +1,13 @@
 from . import cmap
 from . import pvalues
+from ._harmonic_mean import harmonic_mean
 from ._subplots import subplots
 from ._textcolor import rgba2gray, textcolor
 
 
 kwarg_savefig = {
     "facecolor": "white",
-    "dpi": 300,
+    "dpi": 600,
     "bbox_inches": "tight",
     "pad_inches": 0.05,
     "transparent": True,
@@ -25,13 +26,22 @@ dsmat_pref = dict(
 )
 
 
+def order2_interaction_regex(key: int, id_min: int, id_max: int) -> str:
+    regex = f"^X[{key}]$"
+    regex += f"|^X{id_min}X[{id_min + 1}-{id_max}]$" if key == id_min \
+        else f"|^X[{id_min}-{key - 1}]X{key}$|^X{key}X[{id_min}-{id_max}]$"
+    return regex
+
+
 __all__ = [
     kwarg_savefig,
     outputdir,
     heatmap_pref,
     dsmat_pref,
+    order2_interaction_regex,
     cmap,
     pvalues,
+    harmonic_mean,
     rgba2gray,
     subplots,
     textcolor,
