0e00c2dfd178d9e2e55876ba9d19d2239a3d0e9d
yo-aka-gene <yujiokano@keio.jp>
2025-11-29 07:30:54 +0900

    :recycle: refactoring

--- doe_modules/design/d_optimization.R
+++ doe_modules/design/d_optimization.R
@@ -11,28 +11,6 @@ options(warn = -1)
 #   suppressPackageStartupMessages(library(pkg, character.only = TRUE))
 # }
 
-
-# optslist <- list(
-#   make_option(
-#     c("-t", "--tempdir"),
-#     typ = "character",
-#     default = "/home/jovyan/out",
-#     help = "temporary directory to save intermediate files"
-#   ),
-#   make_option(
-#     c("-a", "--add"),
-#     typ = "integer",
-#     default = 0,
-#     help = "number of trials to add to the original design matrix"
-#   )
-# )
-# parser <- OptionParser(option_list = optslist)
-# opts <- parse_args(parser)
-
-# arr <- read_feather(paste0(opts$tempdir, "/arr.feather"), mmap = FALSE)
-# candidate <- read_feather(paste0(opts$tempdir, "/candidate.feather"))
-# n_add <- opts$add
-
 d_optimize_core <- function(dsmatrix, candidate, n_add, random_state) {
     set.seed(random_state)
     
