10ecd19267194545acff3cdc2dc97ce71388975a
yo-aka-gene <yujiokano@keio.jp>
2025-11-29 04:27:01 +0900

    :wastebasket: clean up deprecated codes

--- doe_modules/simulation/__init__.py
+++ doe_modules/simulation/__init__.py
@@ -1,15 +1,10 @@
 from ._abstract import AbstractSimulator
 from ._sim1 import Sim1
-# from ._sim1_minus import Sim1Minus
-# from ._sim1_plus import Sim1Plus
 from ._circuit import Circuit
-# from ._circuit_minus import CircuitMinus
-# from ._circuit_plus import CircuitPlus
 from ._sparse import Sparse
 from ._prototype import Prototype
 from ._test4 import Test4
 from ._test9 import Test9
-from ._nonlinear import NonLinear
 from ._mlr import MLR
 from ._anova_power import anova_power
 from ._theoretical_effects import TheoreticalEffects
@@ -25,18 +20,12 @@ from . import esm9_metrics
 __all__ = [
     AbstractSimulator, 
     Sim1,
-    # Sim1Minus,
-    # Sim1Plus,
     Circuit,
-    # CircuitMinus,
-    # CircuitPlus,
     Sparse,
     Prototype,
     Test4,
     Test9,
-    # NonLinear, 
     MLR,
-    #ff_anova_power,
     anova_power,
     TheoreticalEffects,
     Dunnett,
